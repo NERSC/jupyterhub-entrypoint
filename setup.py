b40badb233a6bcdf5b4f0364a3faf7e2f845e08c
Rollin Thomas <rcthomas@lbl.gov>
2021-10-10 11:06:27 -0700

    Add "manage" template for new/updated entrypoints

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
         author_email='jgeden@lbl.gov',
         url='https://github.com/Josh0823/jupyterhub-entrypoint',
         packages=['jupyterhub_entrypoint', 'jupyterhub_entrypoint.dbi'],
-        data_files=[("share/jupyterhub/entrypoint/templates", 
-            ["templates/index.html", "templates/error-no-tags.html"])]
+        data_files=[("share/jupyterhub/entrypoint/templates", [
+            "templates/index.html",
+            "templates/error-no-tags.html",
+            "templates/manage.html"
+        ])]
 )
