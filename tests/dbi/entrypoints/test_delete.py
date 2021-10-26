e032f8653d18e7be3884a5d770d141889ebafff4
Rollin Thomas <rcthomas@lbl.gov>
2021-10-25 17:17:30 -0700

    Add test for cascading (add, delete, is gone?)

--- tests/dbi/entrypoints/test_delete.py
+++ tests/dbi/entrypoints/test_delete.py
@@ -1,5 +1,6 @@
 
 import pytest
+from sqlalchemy.sql import select
 
 from jupyterhub_entrypoint import dbi
 
@@ -37,3 +38,21 @@ async def test_fails(engine, context_names, entrypoint_args):
         async with engine.begin() as conn:
             output = await dbi.delete_entrypoint(conn, user, "quantum")
 
+@pytest.mark.asyncio
+async def test_cascade(engine, context_names, entrypoint_args):
+    async with engine.begin() as conn:
+        for context_name in context_names:
+            await dbi.create_context(conn, context_name)
+    async with engine.begin() as conn:
+        for args in entrypoint_args:
+            if args[-1]:
+                await dbi.create_entrypoint(conn, *args)
+                break
+    
+    async with engine.begin() as conn:
+        await dbi.delete_entrypoint(conn, *args[:2])
+
+    async with engine.begin() as conn:
+        statement = select(dbi.model.entrypoint_contexts)
+        results = await conn.execute(statement)
+        assert len(list(results)) == 0
