a9c44bc53ce1074ed5b45033ff92c746c0c0261d
Rollin Thomas <rcthomas@lbl.gov>
2021-10-25 17:17:14 -0700

    Enable sqlite to cascade deletes

--- jupyterhub_entrypoint/dbi/__init__.py
+++ jupyterhub_entrypoint/dbi/__init__.py
@@ -1,4 +1,8 @@
 
+from sqlite3 import Connection as SQLite3Connection
+
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from .model import metadata
@@ -26,7 +30,18 @@ from .contexts import (
 )
 
 def async_engine(*args, **kwargs):
-    return create_async_engine(*args, **kwargs)
+    engine = create_async_engine(*args, **kwargs)
+    if engine.name == "sqlite":
+        register_foreign_keys()
+    return engine
+
+def register_foreign_keys(): # pragma: no cover
+    """Enable deletes with cascade in e.g. sqlite"""
+    @event.listens_for(Engine, "connect")
+    def connect(dbi_connection, connection_record):
+        cursor = dbi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 async def init_db(conn, drop_all=False):
     if drop_all:
